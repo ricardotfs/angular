16e87565330553472a7fe7b156dc2a0d06ce63e4
Ricardo Oliveira <ricardo.oliveira@gvp.com.br>
2018-12-04 09:53:04 -0200

    ajuste

--- meat-app-starter-master/src/app/security/login/login.service.ts
+++ meat-app-starter-master/src/app/security/login/login.service.ts
@@ -1,60 +1,76 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http"
-import { Observable } from "rxjs/Observable";
-import { MEAT_API_AUX } from "../../app.api"
-import { User } from "app/security/login/user.model";
-import 'rxjs/add/operator/do'
-import { HttpHeaders } from "@angular/common/http";
-import { HttpParams } from "@angular/common/http";
-import { Router, NavigationEnd } from "@angular/router";
-import 'rxjs/add/operator/filter'
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
+import 'rxjs/Rx';
 
+declare var $: any;
+
+export class Filter {
+    public Key: string;
+    public Value: any;
+    public ComparisonType: FilterComparisonType;
+    constructor() {
+        this.ComparisonType = FilterComparisonType.Equal;
+    }
+}
+
+export class Ordering {
+    public Field: string;
+    public By: any;
+}
+
+export enum FilterComparisonType {
+    Equal,
+    Contains
+}
 
 @Injectable()
 export class LoginService {
 
-    user: User;
-    lastUrl: string
+    private options: RequestOptions;
 
-    constructor(private http: HttpClient, private router: Router) {
-        this.router.events.filter(e => e instanceof NavigationEnd)
-            .subscribe((e: NavigationEnd) => this.lastUrl = e.url)
+    constructor(private http: Http) { }
+
+    get(domain: string, param?: any): Observable<any> {
+        
+        console.log(environment.apiBaseUrl);
+        let url = environment.apiBaseUrl + 'api/' + domain;
+        this.options = this.initializeOptions();
+        if (param) {
+            url += '/' + param;
+        }        
+
+        return this.http.get(url, this.options)
+            .map(result => { return this.successResult(result) })
+            .catch(error => { return this.errorResult(error) });
     }
 
-    logout() {
-        this.user = undefined
+    initializeOptions() {
+        const headers = new Headers({
+          'Content-Type': 'application/json'
+        });
+    
+        return new RequestOptions({ headers: headers });
+      }
+
+    post(domain: string, data: any): Observable<any> {
+        const url = environment.apiBaseUrl + 'api/' + domain;
+        this.options = this.initializeOptions();
+        return this.http.post(url, JSON.stringify(data), this.options)
+          .map(result => { return this.successResult(result) })
+          .catch(error => { return this.errorResult(error) });
+      }
+
+    private successResult(result: Response): Observable<any> {
+        return result.json();
     }
 
-    handleLogin(path: string = this.lastUrl) {
-        this.router.navigate(['/login', btoa(path)])
+    private errorResult(error: Response): Observable<any> {
+        return Observable.throw(error.json());
     }
 
-    isLoggedIn() {
-        return this.user !== undefined;
-    }
-
-    login(email: string, password: string): Observable<User> {
-        const httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin' : '*',
-            }),
-        };
-
-        //return this.http.post<User>(`${MEAT_API_AUX}/login`,{ email: email, password: password },httpOptions)
-        //.map(user => this.user = user);
-        return this.http.get<User>(`${MEAT_API_AUX}/login${1}`)
-
-        //return this.http.post<User>(`${MEAT_API_AUX}/login`, { email: email, password: password }, httpOptions)
-         //   .do(user => this.user = user);
 
 
-        // let headers = new HttpHeaders();
-        // headers = headers.set('Content-Type', 'application/x-www-form-urlencoded')
-        // let params = new HttpParams().set('user', JSON.stringify({email: email,password:password}));
-        // let body = ""
-        // return this.http.post<User>(`${MEAT_API_AUX}/login`,
-        // body,{headers: headers,params:params})
-        //     .do(user => this.user = user);
-    }//
+
 }
