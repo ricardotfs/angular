99117dd44766b47ee5cdbce8ad1447add708b62a
Ricardo Oliveira <ricardo.oliveira@gvp.com.br>
2019-08-14 12:05:04 -0300

    ajuste

---
+++ matador-de-monstro/app.js
@@ -0,0 +1,48 @@
+new Vue({
+    el:"#app",
+    data:{
+        running:false,
+        playerLife:100,
+        monsterLife:100,
+    },
+    computed:{
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods:{
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(especial){
+            this.hurt('monsterLife',5,10,especial)
+            this.hurt('playerLife',7,12,especial)
+        },
+        getRandom(min,max){
+            const value = Math.random() * (max - min) + min
+            
+            return Math.round(value);
+        },
+        hurt(attr,min,max,especial){
+            const plus = especial ? 5:0;
+            const hurt = this.getRandom(min + plus,max + plus)
+            this[attr] = Math.max(this.playerLife - hurt, 0)
+        },
+        heal(min,max){
+            const heal = this.getRandom(min,max)
+            this.playerLife = Math.min(this.playerLife + heal,100)
+        },
+        healAndHurt(){
+            this.heal(10,15)
+            this.hurt('playerLife',7,12,false)
+        }
+
+    },
+    watch:{
+        hasResult(value){
+            if(value) this.running = false
+        }
+    }
+})
