5961b9c8c1001e365c89564dffdac309e7cdd469
ricardotfs <ricardotfs.oliveira@outlook.com>
2020-02-19 16:58:14 -0300

    ajuste

--- WebApi2/Front-End/app/app.js
+++ WebApi2/Front-End/app/app.js
@@ -7,40 +7,40 @@ app.constant('ngAuthSettings', { apiServiceBaseUri: serviceBase });
 app.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/");
     $locationProvider.hashPrefix('');
-    
+
     var noCache = version;
 
     $stateProvider
-      .state('index', {
-          url: "/", // root route
-          views: {
-              "lazyLoadView": {
-                  controller: 'homeController', // This view will use AppCtrl loaded below in the resolve
-                  templateUrl: 'app/views/home.html'
-              }
-          },
-          resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
-              loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
-                  // you can lazy load files for an existing module
-                  return $ocLazyLoad.load(`app/controllers/homeController.js?v=${noCache}`);
-              }]
-          }
+        .state('index', {
+            url: "/", // root route
+            views: {
+                "lazyLoadView": {
+                    controller: 'homeController', // This view will use AppCtrl loaded below in the resolve
+                    templateUrl: 'app/views/home.html'
+                }
+            },
+            resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
+                loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    // you can lazy load files for an existing module
+                    return $ocLazyLoad.load(`app/controllers/homeController.js?v=${noCache}`);
+                }]
+            }
         })
-      .state('login', {
-          url: "/login", // root route
-          views: {
-              "lazyLoadView": {
-                  controller: 'loginController', // This view will use AppCtrl loaded below in the resolve
-                  templateUrl: 'app/views/login.html'
-              }
-          },
-          resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
-              loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
-                  // you can lazy load files for an existing module
-                  return $ocLazyLoad.load(`app/controllers/loginController.js?v=${noCache}`);
-              }]
-          }
-      })
+        .state('login', {
+            url: "/login", // root route
+            views: {
+                "lazyLoadView": {
+                    controller: 'loginController', // This view will use AppCtrl loaded below in the resolve
+                    templateUrl: 'app/views/login.html'
+                }
+            },
+            resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
+                loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    // you can lazy load files for an existing module
+                    return $ocLazyLoad.load(`app/controllers/loginController.js?v=${noCache}`);
+                }]
+            }
+        })
         .state('ticket', {
             url: "/ticket", // root route
             views: {
@@ -53,6 +53,21 @@ app.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
                 loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
                     // you can lazy load files for an existing module
                     return $ocLazyLoad.load(`app/modules/ticket/controller/ticketController.js?v=${noCache}`);
+                }]
+            }
+        })
+        .state('telefone', {
+            url: "/telefone", // root route
+            views: {
+                "lazyLoadView": {
+                    controller: 'telefoneController',
+                    templateUrl: 'app/modules/telefone/view/telefone.html'
+                }
+            },
+            resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
+                loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    // you can lazy load files for an existing module
+                    return $ocLazyLoad.load(`app/modules/telefone/controller/telefoneController.js?v=${noCache}`);
                 }]
             }
         });
---
+++ WebApi2/Front-End/app/modules/telefone/controller/telefoneController.js
@@ -0,0 +1,7 @@
+﻿angular.module('AngularAuthApp').controller('telefoneController',
+    ['$scope', '$location', '$injector', '$ocLazyLoad',
+        function ($scope, $location, $injector, $ocLazyLoad) {
+            $scope.nome = 'Ricardo Oliveira';
+            $scope.email = 'Ricardo@gvp.com';
+            $scope.senha = '123456';
+        }]);
