7effbf6c39bf5a8744fd29e676e3b496182d78f9
ricardotfs <ricardotfs.oliveira@outlook.com>
2020-02-20 10:40:54 -0300

    ajuste

--- WebApi2/Front-End/app/modules/telefone/controller/telefoneController.js
+++ WebApi2/Front-End/app/modules/telefone/controller/telefoneController.js
@@ -4,4 +4,22 @@
             $scope.nome = 'Ricardo Oliveira';
             $scope.email = 'Ricardo@gvp.com';
             $scope.senha = '123456';
+
+           
+
+            $ocLazyLoad.load('/app/modules/telefone/service/telefoneService.js').then(function () {
+                var service = $injector.get('telefoneService');
+                
+                $scope.salvar = function () {
+                    var data = { nome: $scope.nome, email: $scope.email, senha: $scope.senha };
+                    service.salvar(data).then(function (result) {
+                        var mes = result;
+                        $location.path("/home");
+                    })
+                };
+   
+
+            })
+
+
         }]);
--- WebApi2/Front-End/app/modules/telefone/service/telefoneService.js
+++ WebApi2/Front-End/app/modules/telefone/service/telefoneService.js
@@ -1,1 +1,13 @@
-﻿
+﻿angular.module('AngularAuthApp').factory('telefoneService', ['$http', 'ngAuthSettings',
+    function ($http, ngAuthSettings) {
+
+        var serviceBase = ngAuthSettings.apiServiceBaseUri;
+        var objFactory = {};
+
+        objFactory.salvar = function (data) {
+            return $http.post(serviceBase + 'api/telefone/post', data).then(function (response) {
+                return response;
+            })
+        }
+        return objFactory;
+    }]);
