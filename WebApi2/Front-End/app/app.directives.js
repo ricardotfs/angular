cb469f011101b90feb47537ccfb610793b52dc23
ricardotfs <ricardotfs.oliveira@outlook.com>
2020-02-25 20:56:13 -0300

    ajuste

--- WebApi2/Front-End/app/app.directives.js
+++ WebApi2/Front-End/app/app.directives.js
@@ -14,3 +14,19 @@ angular.module('AngularAuthApp').directive('menucliente', function ($interval) {
         }
     };
 });
+
+angular.module('AngularAuthApp').directive('formulariogvp', function ($interval) {
+    return {
+        restrict: "AE",
+        templateUrl: "app/templates/cliente.busca.html",
+        replace: true,
+        scope: {
+            nomepessoa: "=",
+            sobrenome: "=",
+        },
+        link: function (scope, element, dados) {
+            scope.nomepessoa = dados.nomepessoa;
+            scope.sobrenome = dados.sobrenome;
+        }
+    };
+});
