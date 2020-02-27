4a1dbf6b5a152faa039a2e02dd486ba696b51b28
ricardotfs <ricardotfs.oliveira@outlook.com>
2020-02-27 13:40:31 -0300

    ajuste

--- WebApi2/Front-End/app/app.directives.js
+++ WebApi2/Front-End/app/app.directives.js
@@ -14,7 +14,52 @@ angular.module('AngularAuthApp').directive('menucliente', function ($interval) {
         }
     };
 });
+angular.module('AngularAuthApp').directive('editable', function () {
+    return {
+        restrict: "AE",
+        scope: { editable: "=" },
+        require: '?ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            scope.$watch("editable", function () {
+                //var a = valor;
+                
+                if ($(elem[0])[0].type == 'date') {
+                    $(elem[0])[0].value = new Date($(elem[0])[0].value);
+                    //$(elem[0]).editable({
+                    //    validate: function (value) {
+                    //        if ($.trim(value) == "") return "This field is required";
+                    //    }
+                    //});
+                }
+                //var bb = $(elem[0])[0].value;
 
+                //setTimeout(function () {
+                //    var aa = $(elem[0])[0].value
+                //},100)  
+                //if ($(elem[0]).html() != "") {
+                //    $(elem[0]).attr({ "data-title": "Altere o texto que vai aparecer aqui." });
+                //    $(elem[0]).attr({ "data-placement": "bottom" });
+
+                //    $(elem[0]).editable({
+                //        validate: function (value) {
+                //            if ($.trim(value) == "") return "This field is required";
+                //        }
+                //    });
+                //}
+            });
+        }
+    };
+});
+angular.module('AngularAuthApp').directive('tooltip', function () {
+    return {
+        link: function (scope, elem, attrs, ctrl) {
+            if (attrs.tooltip != "") {
+                $(elem[0]).attr({ "title": attrs.tooltip }); $(elem[0]).attr({ "data-html": true });
+                $(elem[0]).tooltip();
+            }
+        }
+    };
+});
 angular.module('AngularAuthApp').directive('formulariogvp', function () {
     try {
         return {
@@ -25,12 +70,7 @@ angular.module('AngularAuthApp').directive('formulariogvp', function () {
                 formulario: "="
             },
             link: function (scope, element, dados) {
-                scope.validType = function (value,tipo) {
-                    if (tipo == 'date')
-                        return new Date(value);
-
-                    return value;
-                };
+           
                 scope.formulario = scope.formulario;
             }
         };
