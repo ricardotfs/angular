b64725e2b5f546a97c9a4666cf5eaba08d6042f1
ricardotfs <ricardotfs.oliveira@outlook.com>
2020-05-13 18:16:55 -0300

    ajuste

---
+++ query.sql
@@ -0,0 +1,9 @@
+select * from  VIVO_PSC_PESQUISAS where ID = 13
+
+select * from VIVO_EMAILTEMPLATE_AUX where ID_TEMPLATE =125
+
+select * from  VIVO_PSC_QUESTOES WHERE ID IN(1)
+
+
+select * from VIVO_PSC_QUESTOES_VALOR where ID_PERGUNTA = 1
+
