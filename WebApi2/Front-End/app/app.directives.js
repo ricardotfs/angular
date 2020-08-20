384815f626a601176b909fedd9ddeb601b67bd56
ricardotfs <ricardotfs.oliveira@outlook.com>
2020-08-20 15:19:43 -0300

    ajuste

--- WebApi2/Front-End/app/app.directives.js
+++ WebApi2/Front-End/app/app.directives.js
@@ -93,11 +93,15 @@ angular.module('AngularAuthApp').directive('adm', function () {
                     });
                 };
 
-                scope.removeNewChoice = function () {
-                    var newItemNo = scope.choices.length - 1;
-                    if (newItemNo !== 0) {
-                        scope.choices.pop();
-                    }
+                scope.removeNewChoice = function (choice) {
+
+                    var removeIndex = scope.choices.map(item => item.id).indexOf(choice.id);
+
+                    scope.choices.splice(removeIndex, 1);
+                   // var newItemNo = scope.choices.length - 1;
+                   // if (newItemNo !== 0) {
+                   //     scope.choices.pop();
+                   // }
                 };
 
                 scope.addNewOption = function (choice) {
